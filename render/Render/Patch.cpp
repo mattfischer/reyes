b00d9b4ab4e909b7d17164cfebbc492c8ab6fbf2
Matt Fischer <mattfischer84@gmail.com>
2015-03-24 22:16:47 -0500

    Estimate dicing divisions based on derivatives of patch center

--- render/Render/Patch.cpp
+++ render/Render/Patch.cpp
@@ -24,8 +24,10 @@ namespace Render {
 
 	Grid Patch::dice(const Config &config) const
 	{
-		int divisions = 16;
-		Grid grid(divisions, divisions);
+		Geo::Vector corners[4] = { point(0, 0), point(0, 3), point(3, 0), point(3, 3) };
+		for(int i = 0; i < 4; i++) {
+			corners[i] = (config.viewport() * config.projection() * corners[i]).project();
+		}
 
 		Geo::Matrix matrix = config.view() * transformation();
 		Geo::Vector points[16];
@@ -35,10 +37,35 @@ namespace Render {
 			}
 		}
 
-		for(int i = 0; i <= divisions; i++) {
-			for(int j = 0; j <= divisions; j++) {
-				float s = float(i) / float(divisions);
-				float t = float(j) / float(divisions);
+		float b[4] = { 0.125f, 0.375f, 0.375f, 0.125f };
+		float d[4] = { -0.75f, -0.75f, 0.75f, 0.75f };
+
+		Geo::Vector du(0, 0, 0, 0);
+		Geo::Vector dv(0, 0, 0, 0);
+		Geo::Vector p(0, 0, 0, 0);
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				du += b[i] * d[j] * points[i * 4 + j];
+				dv += d[i] * b[j] * points[i * 4 + j];
+				p += b[i] * b[j] * points[i * 4 + j];
+			}
+		}
+		Geo::Vector u = p + du;
+		Geo::Vector v = p + dv;
+		u = (config.viewport() * config.projection() * u).project();
+		v = (config.viewport() * config.projection() * v).project();
+		p = (config.viewport() * config.projection() * p).project();
+		du = u - p;
+		dv = v - p;
+
+		int um = int(du.magnitude());
+		int vm = int(dv.magnitude());
+		Grid grid(um, vm);
+
+		for(int i = 0; i <= um; i++) {
+			for(int j = 0; j <= vm; j++) {
+				float s = float(i) / float(um);
+				float t = float(j) / float(vm);
 
 				float Bs[4] = { (1 - s)*(1 - s)*(1 - s), 3 * s*(1 - s)*(1 - s), 3 * s*s*(1 - s), s*s*s };
 				float Bt[4] = { (1 - t)*(1 - t)*(1 - t), 3 * t*(1 - t)*(1 - t), 3 * t*t*(1 - t), t*t*t };
